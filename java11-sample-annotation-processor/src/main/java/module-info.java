61206cb746586adcf0b8fde2ea1b72c57078180d
Florian Brunner <fbrunnerreg@gmx.ch>
2020-02-29 19:45:03 +0100

    - META-INF registration

--- java11-sample-annotation-processor/src/main/java/module-info.java
+++ java11-sample-annotation-processor/src/main/java/module-info.java
@@ -1,10 +1,7 @@
 
-import com.github.puce77.java11annotationprocessortest.annotation.impl.FooApplicationProcessor;
 
 module com.github.puce77.java11annotationprocessortest.annotation {
     exports com.github.puce77.java11annotationprocessortest.annotation;
-
-    provides javax.annotation.processing.Processor with FooApplicationProcessor;
 
     requires java.compiler;
 }
